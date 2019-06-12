a0145a1f604db0270c5aabebeccdf43813dd1d9a
Subin Choi <subinova@hotmail.com>
2019-06-12 14:27:13 +0100

    created function for running the algo in a For loop

--- Random Forest/ggRFSRC_SD.R
+++ Random Forest/ggRFSRC_SD.R
@@ -3,213 +3,155 @@ library(dplyr)
 library(ggplot2)
 library(ggRandomForests)
 library(parallel)
-library(plot3D)
-set.seed(100)
+library(caret)
+
+#Function for repeating RF with different datasets on a loop.
+#make sure the filepath and nrow for data1 are adjusted according to the datafile
+
+rfsrc_in_loop <- function(sampledata){
 
 #Data cleaning (read .dat file and remove index column)
-data1 <-read.delim(file.choose(), sep=(" "),header = FALSE)
-data2 <- data1[,-1]
+  data1 <-read.delim(paste0("/Users/subinieee/Desktop/ggRFSRC plot/N50_P10-50_TV10_R2/Data/SD",sampledata,".dat"), skip=3, sep=(" "),nrows=50, header = FALSE, stringsAsFactors = FALSE)
+  set.seed(100)
+  data2 <- data1[,-1]
 
 #column names
-col_names <-list(1:52)
-col_names <-paste0("","A",col_names[[1]],"")
-colnames(data2) <-col_names
-colnames(data2)[colnames(data2)=="A51"] <- "time"
-colnames(data2)[colnames(data2)=="A52"] <- "event"
-
-#split 50:50 (trial and test)
-seq_len(nrow(data2))
-smp_size <-floor(0.5*nrow(data2))
-train_ind<-sample(seq_len(nrow(data2)), size=smp_size)
-trial <- data2[train_ind,]
-test <- data2[-train_ind,]
+  col_names <-list(1:ncol(data2))
+  col_names <-paste0("","A",col_names[[1]],"")
+  colnames(data2) <-col_names
+  colnames(data2)[colnames(data2)==colnames(data2[ncol(data2)-1])] <- "time"
+  colnames(data2)[colnames(data2)==colnames(data2[ncol(data2)])] <- "event"
+  data2$event[data2$event=="2"] <-"0"
+  data2$event <-as.numeric(data2$event)
 
 # Create the gg_survival object
-gg_dta <- gg_survival(interval="time",
+  gg_dta <- gg_survival(interval="time",
                       censor="event",
                       data=data2,
                       conf.int=0.95)
 
-# Figure3
-plot(gg_dta) +
-  labs(y="Survival Probability", 
+#split data in 50:50 (trial and test)
+  seq_len(nrow(data2))
+  smp_size <-floor(0.5*nrow(data2))
+  train_ind<-sample(seq_len(nrow(data2)), size=smp_size)
+  trial <- data2[train_ind,]
+  test <- data2[-train_ind,]
+
+#creat a folder to contain the results
+  dir.create(path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
+
+# Survival probability vs observation time
+  plot(gg_dta) +
+    labs(y="Survival Probability", 
        x="Observation Time(years)",
        color="Treatment", 
        fill="treatment") +
-  theme(legend.position=c(0.2,0.2)) +
-  coord_cartesian(y=c(0,1.01))
+    theme(legend.position=c(0.2,0.2)) +
+    coord_cartesian(y=c(0,1.01))
 
+  ggsave("surv_obstime.png", plot=last_plot(),path="/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2,"/")
 
-plot(gg_dta, type = "cum_haz") +
-  labs(y = "Cumulative Hazard",
-       x = "Observation Time (years)",
-       color = "Treatment", fill = "Treatment") +
-  theme(legend.position = c(0.2, 0.8)) + 
-  coord_cartesian(ylim = c(-0.02, 1.22))
+#cumulative hazard vs obs time
+  plot(gg_dta, type = "cum_haz") +
+    labs(y = "Cumulative Hazard",
+         x = "Observation Time (years)",
+         color = "Treatment", fill = "Treatment") +
+    theme(legend.position = c(0.2, 0.8)) + 
+    coord_cartesian(ylim = c(-0.02, 1.22))
 
-
+  ggsave("cum_hazard.png", plot=last_plot(),path="/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2,"/")
+  
 #Trial set train (default nodesize is 15 for survival family - the paper used 3)
+  rfsrc_trial <- rfsrc(Surv(time,event) ~ .,
+                       ntree = 1000,
+                       nodesize = 3,
+                       data=trial,
+                       nsplit=10, 
+                       na.action="na.impute", 
+                       tree.err=TRUE, 
+                       importance=TRUE)
 
-rfsrc_trial <- rfsrc(Surv(time,event) ~ .,
-                   nodesize = 3,
-                   data=trial,
-                   nsplit=10, 
-                   na.action="na.impute", 
-                   tree.err=TRUE, 
-                   importance=TRUE)
-
-summary(rfsrc_pbc)
+  capture.output(summary(rfsrc_trial),file=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2,"/summary_trial.txt"))
+  print(rfsrc_trial)
+  capture.output(print(rfsrc_trial),file=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2,"/print_trial.txt"))
 
 #Generalisation error
-gg_error1<-gg_error(rfsrc_trial)
-plot(na.omit(gg_error1))  
+  gg_error1<-gg_error(rfsrc_trial)
+  plot(na.omit(gg_error1))  
 
-#Training set prediction 
+  ggsave("OOB_err.png", plot=last_plot(),path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
+       
 #strCols should be defined "TRUE" for death(event), "FALSE" for censored
 #strCol <- c("TRUE" = "red",
-            "FALSE"="blue")
+#            "FALSE"="blue")
 
-ggRFsrc <-plot(gg_rfsrc(rfsrc_trial),alpha=0.2)+
-  theme(legend.position="none")+
+  ggRFsrc <-plot(gg_rfsrc(rfsrc_trial),alpha=0.2)+
+    theme(legend.position="none")+
   #scale_color_manual(values = strCol)+
-  labs(y="Survival Probability",
-       x="Time(years)")+
-  coord_cartesian(ylim=c(-0.01,1.01))
-show(ggRFsrc)
+    labs(y="Survival Probability",
+         x="Time(years)")+
+    coord_cartesian(ylim=c(-0.01,1.01))
+  show(ggRFsrc)
 
-#make it more interpretable by comparing b/w treatment, groups showing as numbers not the treatment
-
-plot(gg_rfsrc(rfsrc_trial, 
-              by = "treatment")) +
-  theme(legend.position = c(0.2, 0.2)) +
-  labs(y = "Survival Probability", 
-       x = "Time (years)") + 
-  coord_cartesian(ylim = c(-0.01, 1.01))
+    ggsave("rfsrc_trial.png", plot=last_plot(),path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
 
 #test set predictions
 
-rfsrc_test <- predict(rfsrc_data2, 
-                          newdata = test,
-                          na.action = "na.impute",
-                          importance = TRUE)
+  rfsrc_test <- predict(rfsrc_trial,
+                            newdata = test,
+                            na.action = "na.impute",
+                            importance = TRUE)
+  
+  summary_test <- summary(rfsrc_test)
+  capture.output(summary(rfsrc_test),file=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2,"/summary_test.txt"))
 
-summary(rfsrc_test)
-print(rfsrc_test)
+  print_test<-print(rfsrc_test)
+  capture.output(print(rfsrc_test),file=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2,"/print_test.txt"))
 
-plot(gg_rfsrc(rfsrc_test),alpha=0.2)+
+  plot(gg_rfsrc(rfsrc_test),alpha=0.2)+
 #  scale_color_manual(values = strCol)+
-  theme(legend.position="none")+
-  labs(y="Survival Probability",
-       x="Time(years)")+
-  coord_cartesian(ylim=c(-0.01,1.01))
+    theme(legend.position="none")+
+    labs(y="Survival Probability",
+         x="Time(years)")+
+    coord_cartesian(ylim=c(-0.01,1.01))
 
+  ggsave("rfsrc_test.png", plot=last_plot(),path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
 
-#variable importance(lbls=st.labs needs to be defined)
+# variable importance(lbls=st.labs needs to be defined)
 
-plot(gg_vimp(rfsrc_trial))+
-  theme(legend.position=c(p=0.8,0.2))+
-  labs(fill="VIMP > 0")
+  plot(gg_vimp(rfsrc_trial))+
+    theme(legend.position=c(p=0.8,0.2))+
+    labs(fill="VIMP > 0")
 
-#minimal depth - nodesize different, threshold different
+  ggsave("vimp.png", plot=last_plot(),path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
 
-varsel_trial <- var.select(rfsrc_trial)
+# minimal depth - nodesize different, threshold different
 
-gg_md <- gg_minimal_depth(varsel_trial)
-print(gg_md)
-plot(gg_md)
+  varsel_trial <- var.select(rfsrc_trial)
+  varsel_trial
+  gg_md <- gg_minimal_depth(varsel_trial)
+
+  capture.output(print(gg_md), file=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2,"/top_variables.txt"))
+
+  plot(gg_md)
+  ggsave("top_variables.png", plot=last_plot(),path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
 
 #variable selection comparison
 
-plot(gg_minimal_vimp(gg_md))+
-  theme(legend.position = c(0.8,0.2))
+  plot(gg_minimal_vimp(gg_md))+
+    theme(legend.position = c(0.8,0.2))
+  ggsave("var_comparison.png", plot=last_plot(),path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
 
 #variable dependence
 
-ggRFsrc + geom_vline(aes(xintercept=1), 
-                     linetype="dashed")+
-  coord_cartesian(xlim=c(0,5))
+  ggRFsrc + geom_vline(aes(xintercept=1), 
+                       linetype="dashed")+
+    coord_cartesian(xlim=c(0,5))
 
+  ggsave("variable_dependence.png", plot=last_plot(),path=paste0("/Users/subinieee/Desktop/ggRFSRC plot/SD",ncol(data2)-2))
+  }
 
-#plot(gg_v, 
-    xvar=xvar[-1],
-     panel=TRUE,
-     alpha=0.4)+
-  labs(y="Survival")+
-  scale_color_manual(values = strCol)+
-  theme(legend.position="none")+
-  coord_cartesian(ylim=c(-0.05,1.05))
-
-#box plot generated automatically if y values are factors(convert numeric into factors)
-
-#plot(gg_v,
-     xvar=xvar.cat,
-     alpha=0.4)+
-  labs(y="Survival")+
-  scale_color_manual(values = strCol)+
-  theme(legend.position="none")+
-  coord_cartesian(ylim=c(-0.01,1.02))
-
-#Partial dependence
-
-#xvar <-c(xvar, xvar.cat)
-#time_index <- c(which(rfsrc_pbc$time.interest>1)[1]-1,
-                which(rfsrc_pbc$time.interest>3)[1]-1,
-                which(rfsrc_pbc$time.interest>5)[1]-1)
-#partial_pbc <- mclapply(rfsrc_pbc$time.interest[time_index],
-                        function(tm){
-                          plot.variable(rfsrc_pbc, surv.type = "surv",
-                                        time=tm,
-                                        xvar.names=xvar,
-                                        partial=TRUE,
-                                        show.plots = FALSE)
-                        })
-
-
-#gg_dta <-mclapply(partial_pbc, gg_partial)
-#pbc_ggpart <- combine.gg_partial(gg_dta[[1]], 
-                                 gg_dta[[2]],
-                                 lbls=c("1 Year", "3 Years"))
-
-#Figure 17
-
-
-#plot(pbc_ggpart,
-     panel = TRUE) +
-  labs(y="Survival")+
-  theme(legend.position="none")+
-  coord_cartesian(ylim=c(0.43,1.0))
-
-
-#Fiture 18
-
-#ggplot(pbc_ggpart[["edema"]], aes(y=yhat, x=edema, col=group))+
-  geom_boxplot(notch=TRUE,
-               outlier.shape=NA) + # panel=TRUE,
-  labs(x="Edema", y="Survival (%)",
-       color="Time",
-       shape="Time")+
-  theme(legend.position=c(0.1,0.2))
-
-#Variable interactions
-#gg_interaction(rfsrc_pbc)
-
-#plot(ggint, xvar=xvar)
-
-#Conditional dependence plots
-
-# Get variable dependence at 1 year 
-#ggvar <- gg_variable(rfsrc_pbc, time = 1) 
-# For labeling coplot membership
-#ggvar$edema <- paste("edema = ", ggvar$edema, sep = "") 
-# Plot with linear smooth (method argument)
-#var_dep <- plot(ggvar, xvar = "bili",
-                alpha = 0.5) +
-  # geom_smooth(method = "glm",se = FALSE) +
-  labs(y = "Survival",
-       x = "bili") + 
-  theme(legend.position = "none") + 
-  coord_cartesian(y = c(-.01,1.01)) 
-
-#var_dep
-#var_dep+facet_grid(~edema)
+# Run rfsrc for different datasets on the loop 
+nr_p<-c(10,20,30,40,50) 
+for (i in c(1:5)){rfsrc_in_loop(nr_p[i])}
 
