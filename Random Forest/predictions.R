28b788b12a13fc34571f0a4abf602c5a6d07ec3b
paulbarber <p.barber@ucl.ac.uk>
2019-06-17 14:28:19 +0100

    Added script to get survival predictions at a certain time from rfsrc.

---
+++ Random Forest/predictions.R
@@ -0,0 +1,77 @@
+
+# Obtaining predicted survival from rfsrc
+# PRB, 17/06/19
+
+library(ggRandomForests)
+
+#
+# load data and split in to train and test sets
+#
+data("pbc", package = "randomForestSRC")
+
+pbc$years <- pbc$days/365
+pbc$age <- pbc$age/365
+pbc$treatment <- factor(pbc$treatment, 
+                        levels = c(1,2), 
+                        labels = c("DPCA", "placebo"), 
+                        exclude = NA)
+# get rid of days
+pbc <- subset(pbc, select = -c(days))
+
+pbc.trial <- pbc[!is.na(pbc$treatment),]
+pbc.test <- pbc[is.na(pbc$treatment),]
+
+#
+# Train RF on the train set
+#
+rfsrc_pbc <- rfsrc(Surv(years, status) ~ ., data = pbc.trial,
+                   nsplit = 10, na.action = "na.impute",
+                   tree.err = TRUE,importance = TRUE)
+
+#
+# Get predictions on the test set
+#
+rfsrc_pbc_test <- predict(rfsrc_pbc, newdata = pbc.test,
+                          na.action = "na.impute",
+                          importance = TRUE)
+
+#
+# Choose a cut-off time
+#
+# Look at the vector of times which rfsrc produced predictions for
+# Maybe choose the middle time
+middle <- round(length(rfsrc_pbc_test$time.interest)/2)
+t <- rfsrc_pbc_test$time.interest[middle]
+
+#
+# Get the ground truth as a vector of who did survive beyond t
+#
+truth <- pbc.test$years > t
+
+#
+# Get the prediction of who rf thinks will survive > t
+#
+pred <- rfsrc_pbc_test$survival[,middle] > 0.5
+
+#
+# Confusion matrix
+#
+cm <- table(truth, pred)
+
+#
+# Accuracy
+#
+accuracy <- (cm[1,1] + cm[2,2])/sum(cm)
+
+
+
+
+
+
+
+
+
+
+
+
+
