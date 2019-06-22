cfd404c70866a36821bd7370093e9956663aa43a
Subin Choi <subinova@hotmail.com>
2019-06-22 22:05:33 +0100

    script update - confusion matrix compilation for SP and RF

--- Random Forest/ggRFSRC_SD.R
+++ Random Forest/ggRFSRC_SD.R
@@ -7,47 +7,59 @@ library(caret)
 library(readr)
 library(openxlsx)
 
-Err_train <-c(1:3)
-Err_Test <-c(1:3)
 Avg_Err_train<-c(1:5)
-Avg_Err_Test<-c(1:5)
+Avg_Err_test<-c(1:5)
 a<-1
 b<-1
 TV <-10
-N <-50
-P<-c(10,20,30,40,50)
-datasrc <-"~/GitHub/mscproject/Data/EOTy_N100_P10-50_TV10_R1"
-
+N_arr<-c(10,50,100)
+P<-c(20,40,60,80,100)
+for (k in c(1:3)){N<-N_arr[k]
+datasrc <-paste0("~/GitHub/mscproject/Data/Train data/N",N,"_P20-100_TV10")
 #Function for repeating RF with different datasets on a loop.
-rfsrc_in_loop <- function(sampledata){
-  savepath<<-paste0("",datasrc,"/sd",sampledata,"")
+rfsrc_in_loop <- function(p_number){
+  Err_train <<-c(1:3)
+  Err_test <<-c(1:3)
+  savepath<<-paste0("",datasrc,"/RSF",p_number,"")
   for (j in c(1:3)){
 # Data cleaning (read .dat file and remove index column)
-  data1 <-read.delim(paste0(savepath,"/SD",sampledata,".dat"), 
+  set.seed(j)
+  data_train <-read.delim(paste0("",datasrc,"/SD",p_number,".dat"), 
                      skip=3, 
                      sep=(" "),
                      nrows=N, 
                      header = FALSE, 
                      stringsAsFactors = FALSE)
-  set.seed(j)
-  data2 <- data1[,-1]
+  data_train <- data_train[,-1]
+  data_test <-read.delim(paste0("~/GitHub/mscproject/Data/test data/data/SD_test",p_number,".dat"),
+                         skip=3, 
+                         sep=(" "),
+                         nrows=100, 
+                         header = FALSE, 
+                         stringsAsFactors = FALSE)
 
 #column names
-  #colnames(data2) <-st.cols
-  col_names <-list(1:ncol(data2))
+  #colnames(data_train) <-st.cols
+  col_names <-list(1:ncol(data_train))
   col_names <-paste0("","A",col_names[[1]],"")
-  colnames(data2)[length(colnames(data2))-1] <- "time"
-  colnames(data2)[length(colnames(data2))] <- "event"
-  data2$event <-as.numeric(data2$event)
+  colnames(data_train)[length(colnames(data_train))-1] <- "time"
+  colnames(data_train)[length(colnames(data_train))] <- "event"
+  data_train$event <-as.numeric(data_train$event)
+  
+  col_names <-list(1:ncol(data_test))
+  col_names <-paste0("","A",col_names[[1]],"")
+  colnames(data_test)[length(colnames(data_test))-1] <- "time"
+  colnames(data_test)[length(colnames(data_test))] <- "event"
+  data_test$event <-as.numeric(data_test$event)
 
 # Create the gg_survival object
   gg_dta <- gg_survival(interval="time",
                       censor="event",
-                      data=data2,
+                      data=data_train,
                       conf.int=0.95)
 
 #create a folder to contain the results
-  dir.create(path=paste0("",datasrc,sampledata,"/RSF",sampledata,""))
+  dir.create(path=paste0("",datasrc,"/RSF",p_number,""))
 
 # Survival probability vs observation time
   plot(gg_dta) +
@@ -74,10 +86,9 @@ rfsrc_in_loop <- function(sampledata){
   rfsrc_train <- rfsrc(Surv(time,event) ~ .,
                        ntree = 1000,
                        nodesize = 3,
-                       data=data2,
+                       data=data_train,
                        nsplit=10, 
                        na.action="na.impute",
-                       sampsize=25,
                        tree.err=TRUE, 
                        importance=TRUE)
 
@@ -86,8 +97,8 @@ rfsrc_in_loop <- function(sampledata){
   print(rfsrc_train)
   capture.output(print(rfsrc_train),file=paste0("",savepath,"/print_train.txt",""))
 #test set predictions  
-  rfsrc_test <- predict(rfsrc_test, 
-                        newdata = test,
+  rfsrc_test <- predict(rfsrc_train, 
+                        newdata = data_test,
                         na.action = "na.impute",
                         importance = TRUE)
   
@@ -112,7 +123,7 @@ rfsrc_in_loop <- function(sampledata){
     coord_cartesian(ylim=c(-0.01,1.01))
   show(ggRFsrc)
 
-    ggsave("rfsrc_train.png", plot=last_plot(),path=savepath)
+   ggsave("rfsrc_train.png", plot=last_plot(),path=savepath)
 
 # variable importance(lbls=st.labs needs to be defined)
 
@@ -136,16 +147,21 @@ rfsrc_in_loop <- function(sampledata){
   
 #Prediction Accuracy 
   #find cut-off time(median survival time)
-  t<- median(data2$time)
+  t<- median(data_train$time)
   m <- which.min(abs(rfsrc_train$time.interest - t))
   t<- rfsrc_train$time.interest[m]  
+  
+  t2<- median(data_test$time)
+  m2 <- which.min(abs(rfsrc_test$time.interest - t2))
+  t2<- rfsrc_test$time.interest[m2]  
   #
   # Get the ground truth as a vector of who did survive beyond t
-  truth <- data2$time > t
+  truth <- data_train$time > t
+  truth2 <- data_test$time > t2
    #
   # Get the prediction of who rf thinks will survive > t
-  pred_train <- rfsrc_train$survival[,m] >0.5 #rfsrc_train$survival[,m] > 0.5
-  pred_test <- rfsrc_train$survival.oob[,m] >0.5 #rfsrc_train$survival.oob[,m] >0.5
+  pred_train <- rfsrc_train$survival.oob[,m] >0.5 #rfsrc_train$survival[,m] > 0.5
+  pred_test <- rfsrc_test$survival[,m] >0.5 #rfsrc_train$survival.oob[,m] >0.5
   # Confusion matrix
   cm_train <- matrix(c(length(which(truth=="FALSE" & pred_train=="FALSE")),length(which(truth=="FALSE" & pred_train=="TRUE")),
                  length(which(truth=="TRUE" & pred_train=="FALSE")),length(which(truth=="TRUE" & pred_train=="TRUE"))),
@@ -154,8 +170,8 @@ rfsrc_in_loop <- function(sampledata){
   rownames(cm_train) <- c("FALSE","TRUE")
   cm_train <- as.table(cm_train)
   
-  cm_test <- matrix(c(length(which(truth=="FALSE" & pred_test=="FALSE")),length(which(truth=="FALSE" & pred_test=="TRUE")),
-                  length(which(truth=="TRUE" & pred_test=="FALSE")),length(which(truth=="TRUE" & pred_test=="TRUE"))),
+  cm_test <- matrix(c(length(which(truth2=="FALSE" & pred_test=="FALSE")),length(which(truth2=="FALSE" & pred_test=="TRUE")),
+                  length(which(truth2=="TRUE" & pred_test=="FALSE")),length(which(truth2=="TRUE" & pred_test=="TRUE"))),
                 ncol=2,byrow=TRUE)
   colnames(cm_test) <- c("FALSE","TRUE")
   rownames(cm_test) <- c("FALSE","TRUE")
@@ -166,34 +182,35 @@ rfsrc_in_loop <- function(sampledata){
   accuracy_test <-(cm_test[1,1] + cm_test[2,2])/sum(cm_test)
   #collect data in an array(replace 'a'th number in the array after each loop)
   Err_train[a]<<- accuracy_train
-  Err_Test[a]<<- accuracy_test
+  Err_test[a]<<- accuracy_test
   a <<- a+1
-  sampledata<<-sampledata
+  p_number<<-p_number
   }
   #reset the "a" value to keep it =<3
   a<<-1
   #collect average accuary values of the model at different P values
   Avg_Err_train[b]<<- mean(x = Err_train)
-  Avg_Err_Test[b]<<- mean(x=Err_Test)
+  Avg_Err_test[b]<<- mean(x=Err_test)
   b<<-b+1
 }
-
+b<-1
 # Run rfsrc for different datasets on the loop 
 for (i in c(1:5)){rfsrc_in_loop(P[i])}
 
 #create dataframes and combine with old data
-excelpath<-paste0("",datasrc,"/RSF_N",N,"_results.xlsx","")
-df1 <-data.frame(p_n=P/N, test=Avg_Err_Test, train=Avg_Err_train, stringsAsFactors=FALSE)
+excelpath<-"~/GitHub/mscproject/Data/RSF_results.xlsx"
+
+df1 <-data.frame(p_n=P/N, test=Avg_Err_test, train=Avg_Err_train, stringsAsFactors=FALSE)
 df1<-rbind(read.xlsx(excelpath,sheet="P_N"), df1)
-df2 <-data.frame(TV_P=TV/P, test=Avg_Err_Test, train=Avg_Err_train, stringsAsFactors=FALSE)
+df2 <-data.frame(TV_P=TV/P, test=Avg_Err_test, train=Avg_Err_train, stringsAsFactors=FALSE)
 df2<-rbind(read.xlsx(excelpath,sheet = "TV_P"), df2)
-df3 <-data.frame(TV_N=TV/N, test=Avg_Err_Test, train=Avg_Err_train, stringsAsFactors=FALSE)
+df3 <-data.frame(TV_N=TV/N, test=Avg_Err_test, train=Avg_Err_train, stringsAsFactors=FALSE)
 df3<-rbind(read.xlsx(excelpath, sheet="TV_N"), df3)
 
 #push combined data to a new excel file. 
 write.xlsx(df1,excelpath,sheetName="P_N", append=FALSE)
 write.xlsx(df2,excelpath,sheetName="TV_P",append=TRUE)
-write.xlsx(df2,excelpath,sheetName="TV_N", append=TRUE)
+write.xlsx(df3,excelpath,sheetName="TV_N", append=TRUE)
 wb<-createWorkbook()
 addWorksheet(wb, "P_N")
 writeData(wb,sheet="P_N",df1)
@@ -202,4 +219,4 @@ writeData(wb,sheet="TV_P",df2)
 addWorksheet(wb, "TV_N")
 writeData(wb,sheet="TV_N",df3)
 saveWorkbook(wb,excelpath, overwrite = TRUE)
-
+}
--- Random Forest/spconfusiontable.R
+++ Random Forest/spconfusiontable.R
@@ -1,14 +1,51 @@
 library(readr)
 library(openxlsx)
-P<-c(120,140) 
-for (i in c(1:2)){
-sourcedata<-paste0("/Users/subinieee/Documents/GitHub/mscproject/Data/N200_P120-200_TV18_R1/sd",P[i],"/SETCV_L2/confusion_table.txt")
-testt <-read.csv(sourcedata, sep=(" "), header = FALSE)
-a<-testt$V11[1]
-b<-testt$V15[2]
-dfe <-data.frame(trial=paste0("=",a), test=paste0("=",b), stringsAsFactors=FALSE)
-dfe<-rbind(read.xlsx("/Users/subinieee/Desktop/ggRFSRC plot/test.xlsx"), dfe)
+N<-100
+P<-c(20,40,60,80,100) 
+TV<-10
 
-write.xlsx(dfe,"/Users/subinieee/Desktop/ggRFSRC plot/test.xlsx")
+Err_train<-c(1:5)
+Err_test<-c(1:5)
+
+for (i in c(1:5)){
+datasrc_train<-paste0("~/GitHub/mscproject/Data/Train data/confusion table_train/confusion_table_n",N,"_p",P[i],"_tv",TV,".txt")
+datasrc_test<-paste0("~/GitHub/mscproject/Data/Test data/confusion table_test/confusion_table_n",N,"_p",P[i],"_tv",TV,"_test.txt")
+
+conf_train <-read.table(text = gsub("/", "\t", read_lines(datasrc_train)), sep=(""), header = FALSE, nrows = 2)
+conf_test <-read.table(text = gsub("/", "\t", read_lines(datasrc_test)), sep=(""), header = FALSE, nrows = 2)
+
+
+
+a<-conf_train$V7[1]+conf_train$V11[2]
+b<-conf_train$V8[1]+conf_train$V12[2]
+accuracy_train <-a/b
+
+c<-conf_test$V7[1]+conf_test$V11[2]
+d<-conf_test$V8[1]+conf_test$V12[2]
+accuracy_test <-c/d
+
+Err_train[i]<- accuracy_train
+Err_test[i]<- accuracy_test 
 }
 
+#create dataframes and combine with old data
+excelpath<-"~/GitHub/mscproject/Data/SP_results.xlsx"
+df1 <-data.frame(p_n=P/N, test=Err_test, train=Err_train, stringsAsFactors=FALSE)
+df1<-rbind(read.xlsx(excelpath,sheet="P_N"), df1)
+df2 <-data.frame(TV_P=TV/P, test=Err_test, train=Err_train, stringsAsFactors=FALSE)
+df2<-rbind(read.xlsx(excelpath,sheet = "TV_P"), df2)
+df3 <-data.frame(TV_N=TV/N, test=Err_test, train=Err_train, stringsAsFactors=FALSE)
+df3<-rbind(read.xlsx(excelpath, sheet="TV_N"), df3)
+
+#push combined data to a new excel file. 
+write.xlsx(df1,excelpath,sheetName="P_N", append=FALSE)
+write.xlsx(df2,excelpath,sheetName="TV_P",append=TRUE)
+write.xlsx(df3,excelpath,sheetName="TV_N", append=TRUE)
+wb<-createWorkbook()
+addWorksheet(wb, "P_N")
+writeData(wb,sheet="P_N",df1)
+addWorksheet(wb, "TV_P")
+writeData(wb,sheet="TV_P",df2)
+addWorksheet(wb, "TV_N")
+writeData(wb,sheet="TV_N",df3)
+saveWorkbook(wb,excelpath, overwrite = TRUE)
